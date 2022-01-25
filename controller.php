b2a5422f35460b6c51a2aedea550be5d38365b1b
nisino25 <nisino25@gmail.com>
2022-01-26 06:56:45 +0900

    third

---
+++ controller.php
@@ -0,0 +1,77 @@
+public function register(){
+    
+    if(isset($_POST['submit'])){
+      // echo 'sent it ';
+      $validationFlag = true;
+      $validationList = [
+          "email"=> true,
+          "password"=> true,
+      ];
+      
+      
+      
+      
+      $email= $_POST['email']; 
+      $password = $_POST['password'];
+
+      if(!$email){
+        $validationList['email'] = 'メールは必須入力です';
+        $validationFlag = false;
+        
+      }if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $validationList['email'] = 'メールアドレスは正しくご入力ください。';
+        $validationFlag = false;
+      }
+
+      if(!$password){
+        $validationList['password'] = 'パスワードは必須入力です';
+        $validationFlag = false;
+      }
+
+
+      
+     $_SESSION['validationList'] = $validationList ;
+      $_SESSION['validationFlag'] = $validationFlag ;
+    // -------------------------------------------------------------
+
+      if(!$validationFlag){
+        return;
+        echo 'failed';
+      }else{
+
+        
+
+      
+        
+        $email = mysqli_real_escape_string($GLOBALS['connection'],$email );
+        $password = mysqli_real_escape_string($GLOBALS['connection'],$password );
+        
+        
+        $sql = "INSERT INTO users(id, country_id, email, password, role) VALUES (NULL, 0, '$email', '$password', 0)";
+
+        $result =mysqli_query($GLOBALS['connection'], $sql);
+
+        // $this->updateaTable();
+
+
+        
+
+        // INSERT INTO new_table (Foo, Bar, Fizz, Buzz) SELECT Foo, Bar, Fizz, Buzz FROM initial_table
+    
+        if(!$result){
+          die('failed sending data. <br>'. mysqli_error($GLOBALS['connection']));
+          
+        }else{
+          header("Location: ./index.php");
+          die();
+        }
+
+
+
+      }
+
+      
+    }else{
+      // echo 'not yet';
+    }
+  }
---
+++ registration.php
@@ -0,0 +1,82 @@
+<?php
+  require_once(__DIR__ .'/../../Controllers/PlayerController.php');
+  // sendData($params['player']['id']);
+  // session_start();
+  $controller = new PlayerController();
+  // $params = $controller->view();
+  // $countries = $controller->grabCountryList();
+  // $detail = $controller->detail();
+
+  $controller->register();
+
+  $validationList = $_SESSION['validationList'];
+  $validationFlag = $_SESSION['validationFlag'];?>
+<head>
+  <link rel="stylesheet" type="text/css" href="../../public/css/registration.css">
+  <script> 
+    function validateForm() {
+      let name = document.querySelector('.name').value;
+      let kana = document.querySelector('.kana').value;
+      let tel = document.querySelector('.tel').value;
+      let email  = document.querySelector('.email').value;
+      let main = document.querySelector('.main').value;
+
+      let flag = true;
+      let warning = ''
+      if(!flag) alert(`${warning}`);
+      return true; 
+    }
+
+    </script>
+</head>
+<div class="container">
+  <h1>新規登録</h1>
+  <hr>
+  <form name="myform" action="registration.php" method="post" onsubmit="return confirm('送信してもよろしいですか？');">
+
+    <div class="row">
+      <div class="col-25">
+        <label for="email">Email</label>
+      </div>
+      <div class="col-75">
+      <?php 
+          if(isset($_POST['submit'])){
+            if($validationList['email'] !== true){
+              echo '<dd class="error" style="color:red">' . $validationList['email'] . '</dd>';
+              // echo 'weonggg';
+            }
+              
+          }
+        ?> 
+        <input type="text" id="email" name="email" placeholder="メールアドレス..">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-25">
+        <label for="password">パスワード</label>
+      </div>
+      <div class="col-75">
+        <?php 
+          if(isset($_POST['submit'])){
+            if($validationList['password'] !== true){
+              echo '<dd class="error" style="color:red">' . $validationList['password'] . '</dd>';
+              // echo 'weonggg';
+            }
+              
+          }
+        ?> 
+        <input type="text" id="password" name="password" placeholder="パスワード..">
+      </div>
+    </div>
+
+    
+
+    
+    
+    <div class="row">
+      <input type="submit" name="submit">
+      <!-- <dd><button type="submit" name="submit">送　信</button></dd> -->
+    </div>
+  </form>
+</div>
