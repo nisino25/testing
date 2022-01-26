4ae3ecda873cc5cfc175463bbc9808a1ebc5e976
nisino25 <56564923+nisino25@users.noreply.github.com>
2022-01-26 12:00:04 +0900

    Update controller.php

--- controller.php
+++ controller.php
@@ -39,32 +39,55 @@ public function login(){
         echo 'failed';
       }else{
 
+        // start login varificataion ------------------------
+        // if the user exists ------------------------
+        sql = "IF EXISTS (SELECT * FROM users WHERE email = '$email') BEGIN SELECT 1 END ELSE BEGIN SELECT 2 END";
+        $result =mysqli_query($GLOBALS['connection'], $sql);
+            
+        if($result == 2){
+            $validationList['email'] = 'メールアドレスかパスワードが違います';
+            $validationFlag = false;
+        }else{
+            // if the password matches ------------------------
+            sql = "SELECT * FROM users WHERE email = '$email'";
+            $result =mysqli_query($GLOBALS['connection'], $sql);
+
+            if($result !== $email){
+                $validationList['email'] = 'メールアドレスかパスワードが違います';
+                $validationFlag = false;
+            }else{
+                $email = mysqli_real_escape_string($GLOBALS['connection'],$email );
+                $password = mysqli_real_escape_string($GLOBALS['connection'],$password );
+
+
+                $sql = "INSERT INTO users(id, country_id, email, password, role) VALUES (NULL, 0, '$email', '$password', 0)";
+
+                $result =mysqli_query($GLOBALS['connection'], $sql);
+
+                // $this->updateaTable();
+
+
+
+
+                // INSERT INTO new_table (Foo, Bar, Fizz, Buzz) SELECT Foo, Bar, Fizz, Buzz FROM initial_table
+
+                if(!$result){
+                  die('failed sending data. <br>'. mysqli_error($GLOBALS['connection']));
+
+                }else{
+                  header("Location: ./index.php");
+                  die();
+                }
+            }
+            
+            
+        }
+
         
 
       
         
-        $email = mysqli_real_escape_string($GLOBALS['connection'],$email );
-        $password = mysqli_real_escape_string($GLOBALS['connection'],$password );
         
-        
-        $sql = "INSERT INTO users(id, country_id, email, password, role) VALUES (NULL, 0, '$email', '$password', 0)";
-
-        $result =mysqli_query($GLOBALS['connection'], $sql);
-
-        // $this->updateaTable();
-
-
-        
-
-        // INSERT INTO new_table (Foo, Bar, Fizz, Buzz) SELECT Foo, Bar, Fizz, Buzz FROM initial_table
-    
-        if(!$result){
-          die('failed sending data. <br>'. mysqli_error($GLOBALS['connection']));
-          
-        }else{
-          header("Location: ./index.php");
-          die();
-        }
 
 
 
